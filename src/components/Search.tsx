c127624339d5a51eb03b7e5630f03188a389c322
SkyBlaise <zsc990124@gmail.com>
2021-12-11 22:07:12 +0800

    Add a default list

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -2,6 +2,16 @@ import React from 'react';
 import '../App.css';
 
 const Search: React.FC = () => {
+    const list = [
+        { id: '1', name: 'Frontend' },
+        { id: '2', name: 'Backend' },
+        { id: '3', name: 'Relational Database' },
+        { id: '4', name: 'MVC' },
+        { id: '5', name: 'RESTful APIs' },
+        { id: '6', name: 'Ruby on Rails' },
+        { id: '7', name: 'Go' },
+    ];
+
     return (
         <>
             <form action="/" method="get">
@@ -11,6 +21,11 @@ const Search: React.FC = () => {
                 <input type="text" id="header-search" placeholder="Search blog posts" name="s" />
                 <button type="submit">{'Search'}</button>
             </form>
+            <ul>
+                {list.map((item) => (
+                    <li key={item.id}>{item.name}</li>
+                ))}
+            </ul>
         </>
     );
 };
